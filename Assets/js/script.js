0d567d116a9dd118d7cd1816f6c67694bf98a084
Dan <danaaboston@gmail.com>
2022-11-23 19:23:59 -0500

    Renamed IDs within HTML and set up basic functionality for checking if it is before/currently/after the given times within the time blocks

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,23 +2,43 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  var currentTime = dayjs();
+  var blockNumber = $(".time-block").map(function () { return (this.id) }).toArray();
+  console.log(blockNumber);
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  setInterval(checkTime, 1000);
+
+  function checkTime() {
+    console.log("checkTime");
+    for (var i = 0; i < blockNumber.length; i++) {
+      console.log("FOR");
+      if (dayjs().get("hour") < blockNumber.i) {
+        console.log("Test");
+      } else {
+        console.log("FAILURE");
+        console.log(blockNumber[i]);
+      }
+    }
+  }
+
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+
   // TODO: Add code to display the current date in the header of the page.
   setInterval(function () {
     var currentTime = dayjs();
