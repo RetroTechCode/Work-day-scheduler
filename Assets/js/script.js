00660d3e286d491b92316241704430a938b74a91
Dan <danaaboston@gmail.com>
2022-11-25 13:13:55 -0500

    Update date display function, add initial calls for date and checkTime functions, begin adding button functionality

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -12,16 +12,15 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  $(".btn").click(function () {
+    alert("Test");
+  });
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
 
+  // Run the checkTime function when the page is initially loaded.
+  checkTime();
   // Run the checkTime function every second to ensure near real-time updates
   setInterval(checkTime, 1000);
-
   // Checks the current time to update the color coding of the time blocks.
   function checkTime() {
     for (var i = 0; i < blockNumber.length; i++) {
@@ -41,15 +40,18 @@ $(function () {
     }
   }
 
-
-
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
-  // TODO: Add code to display the current date in the header of the page.
-  setInterval(function () {
+
+  // Load the date/time when the page is initially loaded.
+  date();
+  // Update the time/date every second.
+  setInterval(date, 1000);
+  // Set format and display the current date and time.
+  function date() {
     var currentTime = dayjs();
     $("#currentDay").text(currentTime.format("[It is] MMMM D, YYYY [ at ]hh:mm:ssa"));
-  }, 1000);
+  }
 });
